ce92a9afcae33afd8e130960581cedac23076598
nagendra kumar <nagendrakumar4540@gmail.com>
2025-04-19 01:01:14 +0530

    polymorphism assingment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,7 @@
 package com.driver;
 
 public class Main {
- static class Product{
+ public static class Product{
      public int product(int x,int y){
          return x*y;
      }
@@ -18,5 +18,7 @@ public class Main {
      System.out.println(p.product(39, 85));
      System.out.println(p.product(29 ,35,25));
      System.out.println(p.product(96.35,53.09));
+
+
   }
 }
